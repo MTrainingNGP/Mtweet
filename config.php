ecfb422c2a87ec369a7cae6d2cd75d86607ec560
dipti <baisdipti@gmail.com>
2016-10-19 16:08:09 +0530

    update config file

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
     $database = 'maximess';
     mysql_connect($host, $user, $password) or die("failed to connect server"); 
    
-    mysql_connect_db($database) or die("failed to connect database");
+    mysql_select_db($database) or die("failed to connect database");
 
     session_start();
     
