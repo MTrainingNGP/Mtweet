089171d8567d0373e7b4a859456cf40940588ce5
dipti <baisdipti@gmail.com>
2016-10-19 16:28:11 +0530

    modify

--- config.php
+++ config.php
@@ -9,10 +9,5 @@
     mysql_select_db($database) or die("failed to connect database");
 
     session_start();
-<<<<<<< HEAD
-    
-?>
-=======
 
 ?>
->>>>>>> 528ad58d454b29b867b702c706fae5fe49c9f169
